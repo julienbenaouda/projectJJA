b4fc0dd9c46be362a084ee202e586e7f55d6f9d7
Julien Benaouda <julien.benaouda@student.kuleuven.be>
2018-02-25 23:14:41 +0100

    adds a Domain and Test package, part of the Project class and part of the JUnit test for project

---
+++ src/Test/ProjectTest.java
@@ -0,0 +1,45 @@
+package Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import Domain.Project;
+import junit.framework.Assert;
+
+import java.time.*;
+import java.util.ArrayList;
+
+public class ProjectTest {
+	private Project p;
+	private LocalDateTime creation;
+	private LocalDateTime due;
+	private ArrayList<Task> tasks;
+	
+	@Before
+	public void setUp() {
+		creation = LocalDateTime.of(2018, Month.FEBRUARY, 24, 10, 0);
+		due = LocalDateTime.of(2018, Month.march, 15, 12, 30);
+		tasks = new ArrayList<>();
+		p = new Project("test", "test desc", creation, due);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testProject() {
+		Assert.assertEquals("The specified name is not equal to the given name", "test", p.getName());
+		Assert.assertEquals("the descriptions are not equal", "test desc", p.getDescription());
+		Assert.assertEquals("The start dates are not equal", creation, p.getCreationTime());
+		Assert.assertEquals("the due dates are not equal", due, p.getDueTime());
+		Assert.assertEquals("the list of tasks is not equal", tasks, p.getTasks());
+	}
+
+	@Test
+	public void testAddTask() {
+		p.addTask("test", d, 20, startTime, endTime, dependencies, a);
+	}
+
+	@Test
+	public void testGetTask() {
+		fail("Not yet implemented");
+	}
+
+}
