692b54b7cecc7ab5b9773fa374ffadb637282ee8
Alexander Braekevelt <alexander.braekevelt@gmail.com>
2018-02-26 16:23:37 +0100

    Simplify the StatusTest

--- src/test/StatusTest.java
+++ src/test/StatusTest.java
@@ -8,22 +8,7 @@ public class StatusTest {
 
     @Test
     public void testStatus() {
-        Status a = Status.AVAILABLE;
-        Status b = Status.UNAVAILABLE;
-        Status c = Status.FINISHED;
-        Status d = Status.FAILED;
-        Assert.assertNotEquals("All statuses should be unique!", a, b);
-        Assert.assertNotEquals("All statuses should be unique!", a, c);
-        Assert.assertNotEquals("All statuses should be unique!", a, d);
-        Assert.assertNotEquals("All statuses should be unique!", b, a);
-        Assert.assertNotEquals("All statuses should be unique!", b, c);
-        Assert.assertNotEquals("All statuses should be unique!", b, d);
-        Assert.assertNotEquals("All statuses should be unique!", c, a);
-        Assert.assertNotEquals("All statuses should be unique!", c, b);
-        Assert.assertNotEquals("All statuses should be unique!", c, d);
-        Assert.assertNotEquals("All statuses should be unique!", d, a);
-        Assert.assertNotEquals("All statuses should be unique!", d, b);
-        Assert.assertNotEquals("All statuses should be unique!", d, c);
+        Assert.assertTrue("There should be at least one status!", 0 < Status.values().length);
     }
 
 }
