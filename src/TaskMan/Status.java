722abe17aae2afa9eeb6f8d93ed91c09fb55313f
Alexander Braekevelt <alexander.braekevelt@gmail.com>
2018-02-26 15:43:24 +0100

    Implement & test class Status

--- src/TaskMan/Status.java
+++ src/TaskMan/Status.java
@@ -1,10 +1,10 @@
 package TaskMan;
 
 /**
- * Dummy for enum Status.
+ * This class represents the status of a task.
  * @author Alexander Braekevelt
  *
  */
 public enum Status {
-
+    AVAILABLE, UNAVAILABLE, FINISHED, FAILED
 }
---
+++ src/Test/StatusTest.java
@@ -0,0 +1,29 @@
+package Test;
+
+import TaskMan.Status;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StatusTest {
+
+    @Test
+    public void testStatus() {
+        Status a = Status.AVAILABLE;
+        Status b = Status.UNAVAILABLE;
+        Status c = Status.FINISHED;
+        Status d = Status.FAILED;
+        Assert.assertNotEquals("All statuses should be unique!", a, b);
+        Assert.assertNotEquals("All statuses should be unique!", a, c);
+        Assert.assertNotEquals("All statuses should be unique!", a, d);
+        Assert.assertNotEquals("All statuses should be unique!", b, a);
+        Assert.assertNotEquals("All statuses should be unique!", b, c);
+        Assert.assertNotEquals("All statuses should be unique!", b, d);
+        Assert.assertNotEquals("All statuses should be unique!", c, a);
+        Assert.assertNotEquals("All statuses should be unique!", c, b);
+        Assert.assertNotEquals("All statuses should be unique!", c, d);
+        Assert.assertNotEquals("All statuses should be unique!", d, a);
+        Assert.assertNotEquals("All statuses should be unique!", d, b);
+        Assert.assertNotEquals("All statuses should be unique!", d, c);
+    }
+
+}
