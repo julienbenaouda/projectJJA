77717d456aad7b2ae493be78cb27b92f9eddd9fa
Julien Benaouda <julien.benaouda@student.kuleuven.be>
2018-03-04 22:04:23 +0100

    add xml parsing and writing methods to project, change methods conform the class diagram and write/adapt tests

---
+++ src/taskman/Hoofdklasse.java
@@ -0,0 +1,11 @@
+package taskman;
+
+public class Hoofdklasse {
+
+	public static void main(String[] args) {
+		Project p = new Project("test", "descriptiontest", "22/03/2018 12:00", "29/03/2018 22:00");
+		p.saveToStringBIS();
+
+	}
+
+}
