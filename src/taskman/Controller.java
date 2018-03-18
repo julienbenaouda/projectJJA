ef6a79a3f8bf3cd61e89ac6b459549f1d63af4dc
Alexander Braekevelt <alexander.braekevelt@gmail.com>
2018-03-18 15:10:42 +0100

    New constructor for the Controller

--- src/taskman/Controller.java
+++ src/taskman/Controller.java
@@ -2,75 +2,46 @@ package taskman;
 
 import java.nio.file.AccessDeniedException;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
- * This class serves as a controller between the interface and the backend.
- *
+ * This class is responsible for redirecting calls of the user interface to the responsible objects of the backend.
  * @author Alexander Braekevelt
  */
 public class Controller {
 
     /**
-     * Represents the clock with the system time.
+     * Represents the clock.
      */
     private Clock clock;
 
     /**
-     * Represents the projects in the system.
+     * Represents the user.
      */
-    private HashMap<String, Project> projects;
+    private User user;
 
     /**
-     * Create a controller (with minimum time and regular user type).
+     * Represents the projects with their unique ids.
      */
-    public Controller() {
-        this.clock = new Clock();
-        this.projects = new HashMap<>();
-    }
+    private Map<String, Project> projects;
 
     /**
-     * Create a controller (with given time and regular user type).
-     *
-     * @param initialTime the initial system time.
-     * @throws IllegalArgumentException if the new time if before the old time.
-     * @post the time of the system will be set to the given time
+     * Create a Controller for the given objects.
+     * @param clock a Clock.
+     * @param user a User.
+     * @param projects a Collection of Projects.
      */
-    public Controller(String initialTime) throws IllegalArgumentException {
-        this();
-        this.updateSystemTime(initialTime);
-    }
-
-    /**
-     * Create a controller (with given time and given user type).
-     *
-     * @param initialTime     the initial system time.
-     * @param initialUserType the user that will be active.
-     * @throws IllegalArgumentException if the new time if before the old time.
-     * @post the time of the system will be set to the given time
-     * @post the user type will be set to the given user type.
-     */
-    public Controller(String initialTime, String initialUserType) throws IllegalArgumentException {
-        this();
-        this.updateSystemTime(initialTime);
-        this.setUserType(initialUserType);
-    }
-
-    /**
-     * Create a controller with its private variables. (Used to restore from XML.)
-     * @param projects a HashMap with Strings and Projects
-     * @param clock a Clock
-     */
-    private Controller(HashMap<String, Project> projects, Clock clock) {
-        this.projects = projects;
+    public Controller(Clock clock, User user, Collection<Project> projects) {
         this.clock = clock;
+        this.user = user;
+        this.projects = new HashMap<>();
+        for (Project project: projects) {
+            this.projects.put(project.getName(), project);
+        }
     }
 
     /**
      * Returns a list with all project names.
-     *
      * @return a list of strings.
      */
     public List<String> getProjectNames() {
@@ -79,7 +50,6 @@ public class Controller {
 
     /**
      * Returns if a project with the given name exists.
-     *
      * @param name the name to check
      * @return the boolean
      */
@@ -121,7 +91,6 @@ public class Controller {
     /**
      * This method generates a form containing all parameters needed to create a new project.
      * All values are empty and can be filled in, and then passed back to create a project.
-     *
      * @return A map containing all elements that need to be filled in to create a new project
      */
     public HashMap<String,String> getProjectCreationForm() {
@@ -130,7 +99,6 @@ public class Controller {
 
     /**
      * Adds a project with the properties from a given form.
-     *
      * @param form the creation form for the project
      * @throws IllegalArgumentException when one of the parameters is abscent or not valid.
      * @post a project with the properties from a given form will be added to the controller.
@@ -142,7 +110,6 @@ public class Controller {
 
     /**
      * Returns the details of the given project.
-     *
      * @param name the project of which the details should be returned
      * @return the details of the given project
      * @throws IllegalArgumentException if the project does not exist.
@@ -153,7 +120,6 @@ public class Controller {
 
     /**
      * Returns a list of ids of task that belong to a given project.
-     *
      * @param projectName the name of the project
      * @return a list of Integers
      * @throws IllegalArgumentException if the project does not exist.
@@ -164,7 +130,6 @@ public class Controller {
 
     /**
      * Returns the details of the given task
-     *
      * @param projectName the name of the project of the task
      * @param taskId      the id of the task of which the details should be returned.
      * @return the details of the given task
@@ -176,7 +141,6 @@ public class Controller {
 
     /**
      * Returns a list of details of the available tasks of the given project
-     *
      * @param projectName the name of the project of the available tasks
      * @return the details of the available tasks of the given project
      * @throws IllegalArgumentException if the project does not exist
@@ -188,7 +152,6 @@ public class Controller {
     /**
      * This method generates a form containing all parameters needed to create a new taks.
      * All values are empty and can be filled in, and then passed back to create a task.
-     *
      * @return A map containing all elements that need to be filled in to create a new task
      */
     public HashMap<String,String> getTaskCreationForm() {
@@ -197,7 +160,6 @@ public class Controller {
 
     /**
      * Adds a project with the properties from a given form.
-     *
      * @param projectName the project name
      * @param form        the creation form for the project
      * @throws IllegalArgumentException if the project does not exist.
@@ -209,7 +171,6 @@ public class Controller {
 
     /**
      * Return the ID of the latest task.
-     *
      * @return the ID of the latest task
      */
     public Integer getLastTaskID() {
@@ -218,7 +179,6 @@ public class Controller {
 
     /**
      * Sets the alternative of the given task to the given alternative task
-     *
      * @param projectName       the name of the project which holds both tasks
      * @param taskId            the id of the task
      * @param alternativeTaskId the id of the alternative task
@@ -232,7 +192,6 @@ public class Controller {
 
     /**
      * Adds the given dependency to the given task
-     *
      * @param projectName      the name of the project which holds both tasks
      * @param taskId           the id of the task
      * @param dependencyTaskId the id of the dependency
@@ -249,7 +208,6 @@ public class Controller {
     /**
      * This method generates a form containing all parameters needed to update a task status
      * All values are empty and can be filled in, and then passed back to update a task status.
-     *
      * @return A map containing all elements that need to be filled in to update a task status
      */
     public HashMap<String, String> getUpdateTaskStatusForm() {
@@ -258,7 +216,6 @@ public class Controller {
 
     /**
      * Updates the status of the given task.
-     *
      * @param projectName the name of the project of the task
      * @param taskId      the id of the task to update
      * @param form        the HashMap containing the new values necessary to update the task status
@@ -277,7 +234,6 @@ public class Controller {
 
     /**
      * Return the delay between the end time and the estimated end time in minutes.
-     *
      * @param projectName the project name
      * @param taskId      the task id
      * @return a String with the time between the end time and the estimated end time in minutes.
@@ -289,7 +245,6 @@ public class Controller {
 
     /**
      * Return the time of the system
-     *
      * @return the time of the system
      */
     public String getSystemTime() {
@@ -298,7 +253,6 @@ public class Controller {
 
     /**
      * Updates the time of the system
-     *
      * @param newTime the new time of the system
      * @throws DateTimeParseException   if the text cannot be parsed
      * @throws IllegalArgumentException if the new time if before the old time.
@@ -310,7 +264,6 @@ public class Controller {
 
     /**
      * Returns the name of the active user type.
-     *
      * @return a string.
      */
     public String getUserType() {
@@ -319,7 +272,6 @@ public class Controller {
 
     /**
      * Changes the active user type to the given type.
-     *
      * @param user the name of the user type to change to.
      * @throws IllegalArgumentException if the user type does not exist.
      * @post the user type will be set to the given user type.
@@ -330,7 +282,6 @@ public class Controller {
 
     /**
      * Save a controller to an XML file.
-     *
      * @param path the path of the XML file.
      * @throws ImportExportException if the object can't be written to the file.
      */
@@ -346,7 +297,6 @@ public class Controller {
 
     /**
      * Restore a controller from an XML file.
-     *
      * @param path the path of the XML file.
      * @return the restored controller.
      * @throws ImportExportException if the controller can't be created.
