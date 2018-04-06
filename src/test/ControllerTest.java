859d4ecc51b3191fdf6615f599524fea062090ec
Julien Benaouda <julien.benaouda@student.kuleuven.be>
2018-04-06 22:18:01 +0200

    fix tests for the Controller class

--- src/test/ControllerTest.java
+++ src/test/ControllerTest.java
@@ -1,15 +1,42 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.nio.file.AccessDeniedException;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import taskman.Clock;
+import taskman.Controller;
+import taskman.ImportExportException;
+import taskman.ProjectOrganizer;
+import taskman.TaskStatus;
+import taskman.TimeParser;
+import taskman.UserManager;
+
 public class ControllerTest {
 
-    /*
-    TODO: zorgen dat de tests compileren!
+    
+    // TODO: zorgen dat de tests compileren!
 
     private static Controller controller;
+    private Clock clock;
+    private ProjectOrganizer projectOrganizer;
+    private UserManager userManager;
 
     @Before
     public void runBeforeMethod() {
-        controller = new Controller();
+        userManager = new UserManager();
+        userManager.createUser("test", "test", "projectmanager");
+        userManager.login("test", "test");
+        projectOrganizer = new ProjectOrganizer();
+        clock = new Clock();
+        controller = new Controller(clock, userManager, projectOrganizer);
     }
 
     @After
@@ -18,66 +45,49 @@ public class ControllerTest {
     }
 
     @Test
-
     public void constructor() {
-        controller = new Controller("01/02/0003 04:05");
-        Assert.assertEquals("Constructor does not initialize system time!", "01/02/0003 04:05", controller.getSystemTime());
-        controller = new Controller("01/02/0003 04:05", "DEVELOPER");
-        Assert.assertEquals("Constructor does not initialize system time!", "01/02/0003 04:05", controller.getSystemTime());
-        Assert.assertEquals("Constructor does not initialize user type!", "DEVELOPER", controller.getUserType());
+    	userManager.createUser("test", "myPassword", "developer");
+    	userManager.login("test", "myPassword");
+        assertEquals("Constructor does not initialize system time!", TimeParser.convertLocalDateTimeToString(clock.getTime()), controller.getSystemTime());
+        assertEquals("Constructor does not initialize user type!", "test", controller.getCurrentUserName());
     }
 
     @Test
-    public void project_and_task() throws AccessDeniedException {
+    public void project_and_task() {
         // Project and task are only tested to make sure that the controller works correctly.
         // More extended tests are located in separate test classes.
+        userManager.createUser("pm", "test", "projectmanager");
+        userManager.createUser("d", "test", "developer");
+        userManager.login("pm", "test");
 
         String projectName = "test name";
 
-        Assert.assertFalse("A project already exists when creating the controller!", controller.projectExists("test name"));
-        Assert.assertEquals("Initial project list should be empty!", 0, controller.getProjectNames().size());
-        HashMap<String,String> projectForm = controller.getProjectCreationForm();
-        projectForm.put("name", projectName);
-        projectForm.put("description", "test description");
-        projectForm.put("creationTime", "01/02/2003 04:05");
-        projectForm.put("dueTime", "06/07/2008 09:10");
-        controller.createProject(projectForm);
-        Assert.assertTrue("The added project does not exist!", controller.projectExists(projectName));
-        Assert.assertEquals("Project list should contain one project!", 1, controller.getProjectNames().size());
-        Assert.assertEquals("The project name is incorrect!", projectName, controller.getProjectNames().get(0));
-        Assert.assertNotNull("Project details cannot be null!", controller.getProjectDetails(projectName));
+        assertEquals("Initial project list should be empty!", 0, projectOrganizer.getProjectNames().size());
+        LocalDateTime creation = LocalDateTime.of(2003, Month.FEBRUARY, 1, 4, 5);
+        LocalDateTime due = LocalDateTime.of(2008, Month.JULY, 6, 9, 10);
+        controller.createProject(projectName, "test description", creation, due);
+        assertTrue("The added project does not exist!", controller.projectExists(projectName));
+        assertEquals("Project list should contain one project!", 1, projectOrganizer.getProjectNames().size());
+        assertEquals("The project name is incorrect!", projectName, controller.getProjectNames().get(0));
+        assertNotNull("Project details cannot be null!", controller.getProjectDetails(projectName));
 
-        Assert.assertEquals("Project already has tasks!", 0, controller.getTasksOfProject(projectName).size());
-        HashMap<String,String> taskForm = controller.getTaskCreationForm();
-        taskForm.put("description", "test task description");
-        taskForm.put("estimatedDuration", "709");
-        taskForm.put("acceptableDeviation", "1.345");
-        controller.addTask(projectName, taskForm);
-        Assert.assertEquals("The task is not added!", 1, controller.getTasksOfProject(projectName).size());
-        Integer taskId = controller.getLastTaskID();
-        Assert.assertNotNull("Task details cannot be null!", controller.getTaskDetails(projectName, taskId));
+        assertEquals("Project already has tasks!", 0, controller.getTasksOfProject(projectName).size());
+        controller.addTask(projectName, "test task description", 709l, 1.345);
+        assertEquals("The task is not added!", 1, controller.getTasksOfProject(projectName).size());
+        int taskId = 0;
+        assertNotNull("Task details cannot be null!", controller.getTaskDetails(projectName, taskId));
 
-        controller.addTask(projectName, taskForm);
-        Integer dependencyTaskId = controller.getLastTaskID();
-        Assert.assertNotEquals("The last task id is not updated!", taskId, dependencyTaskId);
+        controller.addTask(projectName, "test task description", 888l, 1.45);
+        int dependencyTaskId = 1;
         controller.addDependencyToTask(projectName, taskId, dependencyTaskId);
 
-        HashMap<String,String> updateForm = controller.getUpdateTaskStatusForm();
-        updateForm.put("startTime", "01/02/2003 05:05");
-        updateForm.put("endTime", "01/01/2100 00:00");
-        updateForm.put("status", "FAILED");
-        try {
-            User.setUserType("REGULARUSER");
-            controller.updateTaskStatus(projectName, taskId, updateForm);
-            Assert.fail("A regular user should not be able to update a task status!");
-        } catch (Exception e) {
-            Assert.assertEquals("Wrong exception!", AccessDeniedException.class, e.getClass());
-        }
-        User.setUserType("DEVELOPER");
-        controller.updateTaskStatus(projectName, taskId, updateForm);
+        userManager.login("d", "test");
+        LocalDateTime startTime = LocalDateTime.of(2003, Month.JANUARY, 1, 5, 5);
+        LocalDateTime endTime = LocalDateTime.of(2100, Month.JANUARY, 1, 0, 0);
+        controller.updateTaskStatus(projectName, taskId, startTime, endTime, TaskStatus.FAILED);
 
-        controller.addTask(projectName, taskForm);
-        Integer alternativeTaskId = controller.getLastTaskID();
+        controller.addTask(projectName, "test", 200l, 1.1);
+        Integer alternativeTaskId = 2;
         controller.addAlternativeToTask(projectName, taskId, alternativeTaskId);
     }
 
@@ -85,26 +95,23 @@ public class ControllerTest {
     public void system_time() {
         String time = controller.getSystemTime();
         String newTime = "09/03/2018 17:13";
-        Assert.assertNotEquals("Initial time is equal to example time!", newTime, time);
+        assertNotEquals("Initial time is equal to example time!", newTime, time);
         controller.updateSystemTime(newTime);
-        Assert.assertEquals("Time is not updated!", newTime, controller.getSystemTime());
+        assertEquals("Time is not updated!", newTime, controller.getSystemTime());
         try {
             controller.updateSystemTime(time);
-            Assert.fail("Time should not be updated to the past!");
+            fail("Time should not be updated to the past!");
         }
         catch (Exception e) {
-            Assert.assertEquals("Wrong exception when updating time to past! (" + e.getMessage() + ")", IllegalArgumentException.class, e.getClass());
+            assertEquals("Wrong exception when updating time to past! (" + e.getMessage() + ")", IllegalArgumentException.class, e.getClass());
         }
     }
 
     @Test
     public void user() {
-        Assert.assertNotNull("User cannot be null!", controller.getUserType());
-        Assert.assertNotEquals("User cannot be ''!", "", controller.getUserType());
-        controller.setUserType("REGULARUSER");
-        Assert.assertEquals("The user type isn't correct!", "REGULARUSER", controller.getUserType());
-        controller.setUserType("DEVELOPER");
-        Assert.assertEquals("The user type isn't correct!", "DEVELOPER", controller.getUserType());
+        assertNotNull("User cannot be null!", controller.getCurrentUserName());
+        assertNotEquals("User cannot be ''!", "", controller.getCurrentUserName());
+        assertEquals("The user type isn't correct!", "test", controller.getCurrentUserName());
     }
 
     private void deleteFile(String path) throws AccessDeniedException {
@@ -122,13 +129,13 @@ public class ControllerTest {
         deleteFile(path);
         controller.exportSystem(path);
         System.out.println("A file is temporally saved to '" + path + "'");
-        Assert.assertTrue("XML file cannot be saved!", new File(path).exists());
+        assertTrue("XML file cannot be saved!", new File(path).exists());
         controller = null;
         controller = Controller.importSystem(path);
         deleteFile(path);
         System.out.println("A file is deleted from '" + path + "'");
-        Assert.assertNotNull("Controller cannot be restored!", controller);
+        assertNotNull("Controller cannot be restored!", controller);
     }
-    */
+    
 
 }
