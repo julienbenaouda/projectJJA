8ac4ad3250e2f8c210de40200dbb790473cc63ab
soeptas <soeptas>
2018-04-11 10:09:18 +0200

    create and implement class

--- src/taskman/Backend/resource/ResourceItem.java
+++ src/taskman/Backend/resource/ResourceItem.java
@@ -1,4 +1,18 @@
 package taskman.Backend.resource;
 
-public class ResourceItem {
+import java.time.LocalDateTime;
+
+public class ResourceItem implements Resource {
+
+    @Override
+    public boolean isAvailable(LocalDateTime startTime, LocalDateTime endTime) {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public boolean isAvailable(LocalDateTime time) {
+        // TODO
+        return false;
+    }
 }
