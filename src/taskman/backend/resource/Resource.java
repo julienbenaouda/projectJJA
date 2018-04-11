df9c9c2e5899388a9054204dae78c4d64c8e2fdb
soeptas <soeptas>
2018-04-11 12:12:52 +0200

    implement interface + minor changes

--- src/taskman/backend/resource/Resource.java
+++ src/taskman/backend/resource/Resource.java
@@ -12,16 +12,19 @@ import java.util.ArrayList;
  *
  */
 public interface Resource {
+
 	/**
-	 * returns the first available time to start the task
-	 * @param Time the time to start searching from
+	 * Returns the first available time to start the task.
+	 *
+	 * @param time the time to start searching from
 	 * @param resources the list of resources to search in
 	 * @return The first available time to plan the task
 	 */
 	LocalDateTime firstAvailableTime(LocalDateTime time, ArrayList<Resource> resources);
 	
 	/**
-	 * returns the type of the resource
+	 * Returns the type of the resource.
+	 *
 	 * @return the type of the resource
 	 */
 	public ResourceType getType();
--- src/taskman/backend/resource/ResourceItem.java
+++ src/taskman/backend/resource/ResourceItem.java
@@ -3,7 +3,22 @@ package taskman.backend.resource;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+/**
+ * Class representing a resource item.
+ *
+ * @author Jeroen Van Der Donckt, Julien Benaouda
+ */
 public class ResourceItem implements Resource {
+
+    /**
+     * Creates a new resource item with the given resource type.
+     *
+     * @param type the resource type of the resource item
+     * @post a new resource item is created with given resource type
+     */
+    public ResourceItem(ResourceType type){
+        setType(type);
+    }
 
 	@Override
 	public LocalDateTime firstAvailableTime(LocalDateTime time, ArrayList<Resource> resources) {
@@ -11,9 +26,23 @@ public class ResourceItem implements Resource {
 		return null;
 	}
 
-	@Override
-	public ResourceType getType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * Represents the resource type of the resource item.
+     */
+	private ResourceType type;
+	
+    @Override
+    public ResourceType getType() {
+        return type;
+    }
+
+    /**
+     * Sets the resource type of the resource item to the given type.
+     *
+     * @param type the resource type of the resource type
+     * @post the resource type of the resource item is set to the given type
+     */
+	private void setType(ResourceType type) {
+	    this.type = type;
+    }
 }
