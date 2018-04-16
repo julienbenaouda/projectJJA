26e646a476151a570dcdb98df0d41188a5094a23
soeptas <boebievdd@gmail.com>
2018-04-16 14:19:53 +0200

    minor changes

--- src/taskman/backend/resource/ResourceType.java
+++ src/taskman/backend/resource/ResourceType.java
@@ -129,10 +129,9 @@ public class ResourceType {
 				numberAvailable += 1;
 			}
 		}
-		if (numberAvailable >= amount){
+		if (numberAvailable >= amount) {
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
@@ -173,6 +172,6 @@ public class ResourceType {
 		if(weekDay < 0 || weekDay > 6) {
 			throw new IllegalArgumentException("the number of the week day must be between 0 and 6");
 		}
-		availability.put(weekDay, availabilityPeriod)
+		availability.put(weekDay, availabilityPeriod);
 	}
 }
