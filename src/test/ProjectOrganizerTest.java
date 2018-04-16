f1c3e414c0e8a60090450e898749a07e6002cc24
Julien Benaouda <julien.benaouda@student.kuleuven.be>
2018-04-16 18:00:54 +0200

    add test for the ProjectOrganizer class

---
+++ src/test/ProjectOrganizerTest.java
@@ -0,0 +1,56 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import taskman.backend.project.Project;
+import taskman.backend.project.ProjectOrganizer;
+import taskman.backend.user.ProjectManager;
+
+public class ProjectOrganizerTest {
+	private ProjectOrganizer organizer;
+
+	@Before
+	public void setUp() {
+		organizer = new ProjectOrganizer();
+	}
+	
+	@Test
+	public void testProjectOrganizer() {
+		assertEquals(0, organizer.getProjectNames().size());
+	}
+
+	@Test
+	public void testProjectExists_true() {
+		LocalDateTime creationTime = LocalDateTime.of(2018, Month.JULY, 26, 12, 0);
+		LocalDateTime dueTime = LocalDateTime.of(2018, Month.JULY, 26, 19, 0);
+		ProjectManager user = new ProjectManager("test", "test");
+		organizer.createProject("test", "testdesc", creationTime, dueTime, user);
+		assertTrue(organizer.projectExists("test"));
+	}
+	
+	@Test
+	public void testProjectExists_false() {
+		assertFalse(organizer.projectExists("test"));
+	}
+
+
+	@Test
+	public void testCreateProject() {
+		LocalDateTime creationTime = LocalDateTime.of(2018, Month.JULY, 26, 12, 0);
+		LocalDateTime dueTime = LocalDateTime.of(2018, Month.JULY, 26, 19, 0);
+		ProjectManager user = new ProjectManager("test", "test");
+		organizer.createProject("test", "testdesc", creationTime, dueTime, user);
+		Project p = organizer.getProject("test");
+		assertEquals("test", p.getName());
+		assertEquals("testdesc", p.getDescription());
+		assertEquals(creationTime, p.getCreationTime());
+		assertEquals(dueTime, p.getDueTime());
+	}
+
+}
