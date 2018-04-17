16bb4e67b765f0d0db011618917ebf3813e02ee8
Julien Benaouda <julien.benaouda@student.kuleuven.be>
2018-04-17 14:23:02 +0200

    adds tests for the parsing from xml

--- src/test/backend/importExport/XmlObjectTest.java
+++ src/test/backend/importExport/XmlObjectTest.java
@@ -76,7 +76,47 @@ public class XmlObjectTest {
 
 	@Test
 	public void testFromXMLString() {
-		fail("Not yet implemented");
+		controller.createUser("test", "test", "project manager", null);
+		controller.login("test", "test");
+		LocalDateTime creationTime = LocalDateTime.of(2018, Month.JULY, 26, 0, 0);
+		LocalDateTime dueTime = LocalDateTime.of(2018, Month.JULY, 26, 23, 0);
+		po.createProject("test", "test", creationTime, dueTime, um.getCurrentUser());
+		controller.createTask("test", "test", "test", 60l, 5.0);
+		rm.addResourceType("type1");
+		rm.getResourceType("type1").createResource("testResource");
+		LocalTime startBreak = LocalTime.of(12, 0);
+		um.createUser("testdeveloper", "test", "developer", startBreak, rm);
+		controller.addRequirementToTask("test", "test", "type1", 1);
+		controller.addRequirementToTask("test", "test", "developer", 1);
+		Pair<String, String> p1 = new Pair("developer", "test");
+		Pair<String, String> p2 = new Pair("type1", "testResource");
+		ArrayList<Pair<String, String>> resources = new ArrayList<>();
+		resources.add(p1);
+		resources.add(p2);
+		LocalDateTime startTime = LocalDateTime.of(2018, Month.JULY, 26, 0, 0);
+		controller.plan("test", "test", resources, startTime);
+		rm.createConstraint("== type1 1");
+		String xml = "";
+		try {
+			xml = o.toXMLString();
+		} catch(Exception e) {
+			fail("test failed " +e.getMessage());
+		}
+		XmlObject obj = null;
+		try {
+			obj = XmlObject.fromXMLString(xml);
+		} catch (Exception e) {
+			fail("test failed: " +e.getMessage());
+		}
+		po = obj.getProjectOrganizer();
+		um = obj.getUserManager();
+		rm = obj.getResourceManager();
+		c = obj.getClock();
+		assertNotNull(po.getProject("test"));
+		assertNotNull(po.getProject("test").getTask("test"));
+		assertNotNull(um.getCurrentUser());
+		assertNotNull(rm.getResourceType("type1"));
+		assertNotNull(rm.getResourceType("type1").getResource("testResource"));
 	}
 
 }
