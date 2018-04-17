d0020a55a32c1ab6670ee5ad52dfdab26b3b738f
Alexander Braekevelt <alexander.braekevelt@gmail.com>
2018-04-17 15:15:26 +0200

    Update constraint parser

--- src/taskman/backend/constraint/ConstraintComponent.java
+++ src/taskman/backend/constraint/ConstraintComponent.java
@@ -3,13 +3,15 @@ package taskman.backend.constraint;
 import taskman.backend.resource.ResourceManager;
 import taskman.backend.resource.ResourceType;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Map;
 
 /**
  * Interface representing the constraint component.
  * Note: We apply here the Composite Pattern
  *
- * @author Jeroen Van Der Donckt
+ * @author Jeroen Van Der Donckt, Alexander Braekevelt
  */
 public interface ConstraintComponent {
 
@@ -33,70 +35,114 @@ public interface ConstraintComponent {
         if (string == null || string.isEmpty()) {
             throw new IllegalArgumentException("Constraint cannot be empty!");
         }
-        String[] substrings = string.split(" ");
-        switch (substrings[0]) {
-            case "&&":
-                if (substrings.length != 3) {
-                    throw new IllegalArgumentException(substrings[0] + " cannot be applied to " + (substrings.length - 1) + " arguments!");
-                } else {
-                    return new AndConstraint(parseConstraint(substrings[1], manager), parseConstraint(substrings[2], manager));
-                }
-            case "||":
-                if (substrings.length != 3) {
-                    throw new IllegalArgumentException(substrings[0] + " cannot be applied to " + (substrings.length - 1) + " arguments!");
-                } else {
-                    return new OrConstraint(parseConstraint(substrings[1], manager), parseConstraint(substrings[2], manager));
-                }
-            case "!":
-                if (substrings.length != 2) {
-                    throw new IllegalArgumentException(substrings[0] + " cannot be applied to " + (substrings.length - 1) + " arguments!");
-                } else {
-                    return new NotConstraint(parseConstraint(substrings[1], manager));
-                }
-            case "if":
-                if (substrings.length != 3) {
-                    throw new IllegalArgumentException(substrings[0] + " cannot be applied to " + (substrings.length - 1) + " arguments!");
-                } else {
-                    return new IfThenConstraint(parseConstraint(substrings[1], manager), parseConstraint(substrings[2], manager));
-                }
-            case "==":
-                if (substrings.length != 3) {
-                    throw new IllegalArgumentException(substrings[0] + " cannot be applied to " + (substrings.length - 1) + " arguments!");
-                } else {
-                    return new Constraint(manager.getResourceType(substrings[1]), AmountComparator.EQUALS, Integer.parseInt(substrings[2]));
-                }
-            case "!=":
-                if (substrings.length != 3) {
-                    throw new IllegalArgumentException(substrings[0] + " cannot be applied to " + (substrings.length - 1) + " arguments!");
-                } else {
-                    return new Constraint(manager.getResourceType(substrings[1]), AmountComparator.NOT_EQUALS, Integer.parseInt(substrings[2]));
-                }
-            case ">":
-                if (substrings.length != 3) {
-                    throw new IllegalArgumentException(substrings[0] + " cannot be applied to " + (substrings.length - 1) + " arguments!");
-                } else {
-                    return new Constraint(manager.getResourceType(substrings[1]), AmountComparator.GREATER_THAN, Integer.parseInt(substrings[2]));
-                }
-            case ">=":
-                if (substrings.length != 3) {
-                    throw new IllegalArgumentException(substrings[0] + " cannot be applied to " + (substrings.length - 1) + " arguments!");
-                } else {
-                    return new Constraint(manager.getResourceType(substrings[1]), AmountComparator.GREATER_THAN_OR_EQUALS, Integer.parseInt(substrings[2]));
-                }
-            case "<":
-                if (substrings.length != 3) {
-                    throw new IllegalArgumentException(substrings[0] + " cannot be applied to " + (substrings.length - 1) + " arguments!");
-                } else {
-                    return new Constraint(manager.getResourceType(substrings[1]), AmountComparator.SMALLER_THAN, Integer.parseInt(substrings[2]));
-                }
-            case "<=":
-                if (substrings.length != 3) {
-                    throw new IllegalArgumentException(substrings[0] + " cannot be applied to " + (substrings.length - 1) + " arguments!");
-                } else {
-                    return new Constraint(manager.getResourceType(substrings[1]), AmountComparator.SMALLER_THAN_OR_EQUALS, Integer.parseInt(substrings[2]));
-                }
-            default:
-                throw new IllegalArgumentException("Invalid constraint operator:" + substrings[0]);
+        String[] parts = string.split(" ");
+        Deque<String> argumentStack = new ArrayDeque<>();
+        Deque<ConstraintComponent> constraintStack = new ArrayDeque<>();
+        for (int i = parts.length - 1; i >= 0; i--) {
+            switch (parts[i]) {
+                case "and":
+                    if (constraintStack.size() < 2) {
+                        throw new IllegalArgumentException(parts[i] + " cannot be applied to " + constraintStack.size() + " arguments!");
+                    } else {
+                        constraintStack.push(new AndConstraint(constraintStack.pop(), constraintStack.pop()));
+                    }
+                    break;
+                case "or":
+                    if (constraintStack.size() < 2) {
+                        throw new IllegalArgumentException(parts[i] + " cannot be applied to " + constraintStack.size() + " arguments!");
+                    } else {
+                        constraintStack.push(new OrConstraint(constraintStack.pop(), constraintStack.pop()));
+                    }
+                    break;
+                case "not":
+                    if (constraintStack.size() < 1) {
+                        throw new IllegalArgumentException(parts[i] + " cannot be applied to " + constraintStack.size() + " arguments!");
+                    } else {
+                        constraintStack.push(new NotConstraint(constraintStack.pop()));
+                    }
+                    break;
+                case "if":
+                    if (constraintStack.size() < 2) {
+                        throw new IllegalArgumentException(parts[i] + " cannot be applied to " + constraintStack.size() + " arguments!");
+                    } else {
+                        constraintStack.push(new IfThenConstraint(constraintStack.pop(), constraintStack.pop()));
+                    }
+                    break;
+                case "==":
+                    if (argumentStack.size() < 2) {
+                        throw new IllegalArgumentException(parts[i] + " cannot be applied to " + argumentStack.size() + " arguments!");
+                    } else {
+                        constraintStack.push(new Constraint(
+                                manager.getResourceType(argumentStack.pop()),
+                                AmountComparator.EQUALS,
+                                Integer.parseInt(argumentStack.pop())
+                        ));
+                    }
+                    break;
+                case "!=":
+                    if (argumentStack.size() < 2) {
+                        throw new IllegalArgumentException(parts[i] + " cannot be applied to " + argumentStack.size() + " arguments!");
+                    } else {
+                        constraintStack.push(new Constraint(
+                                manager.getResourceType(argumentStack.pop()),
+                                AmountComparator.NOT_EQUALS,
+                                Integer.parseInt(argumentStack.pop())
+                        ));
+                    }
+                    break;
+                case ">":
+                    if (argumentStack.size() < 2) {
+                        throw new IllegalArgumentException(parts[i] + " cannot be applied to " + argumentStack.size() + " arguments!");
+                    } else {
+                        constraintStack.push(new Constraint(
+                                manager.getResourceType(argumentStack.pop()),
+                                AmountComparator.GREATER_THAN,
+                                Integer.parseInt(argumentStack.pop())
+                        ));
+                    }
+                    break;
+                case ">=":
+                    if (argumentStack.size() < 2) {
+                        throw new IllegalArgumentException(parts[i] + " cannot be applied to " + argumentStack.size() + " arguments!");
+                    } else {
+                        constraintStack.push(new Constraint(
+                                manager.getResourceType(argumentStack.pop()),
+                                AmountComparator.GREATER_THAN_OR_EQUALS,
+                                Integer.parseInt(argumentStack.pop())
+                        ));
+                    }
+                    break;
+                case "<":
+                    if (argumentStack.size() < 2) {
+                        throw new IllegalArgumentException(parts[i] + " cannot be applied to " + argumentStack.size() + " arguments!");
+                    } else {
+                        constraintStack.push(new Constraint(
+                                manager.getResourceType(argumentStack.pop()),
+                                AmountComparator.SMALLER_THAN,
+                                Integer.parseInt(argumentStack.pop())
+                        ));
+                    }
+                    break;
+                case "<=":
+                    if (argumentStack.size() < 2) {
+                        throw new IllegalArgumentException(parts[i] + " cannot be applied to " + argumentStack.size() + " arguments!");
+                    } else {
+                        constraintStack.push(new Constraint(
+                                manager.getResourceType(argumentStack.pop()),
+                                AmountComparator.SMALLER_THAN_OR_EQUALS,
+                                Integer.parseInt(argumentStack.pop())
+                        ));
+                    }
+                    break;
+                default:
+                    argumentStack.push(parts[i]);
+                    break;
+            }
+        }
+        if (argumentStack.size() == 0 && constraintStack.size() == 1) {
+            return constraintStack.pop();
+        } else {
+            throw new IllegalArgumentException("Invalid constraint expression!");
         }
     }
 
