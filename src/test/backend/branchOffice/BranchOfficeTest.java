b4559a8f9a24b6bc26dde09f1e3ede9040483fcb
soeptas <boebievdd@gmail.com>
2018-05-22 15:32:40 +0200

    update tests

--- src/test/backend/branchOffice/BranchOfficeTest.java
+++ src/test/backend/branchOffice/BranchOfficeTest.java
@@ -15,7 +15,6 @@ public class BranchOfficeTest {
 	ResourceManager resourceManager;
 	UserManager userManager;
 	BranchOffice branchOffice1;
-	BranchOffice branchOffice2;
 
 
 	@Before
@@ -24,7 +23,6 @@ public class BranchOfficeTest {
 		resourceManager = new ResourceManager();
 		userManager = new UserManager();
 		branchOffice1 = new BranchOffice("office 1");
-		branchOffice2 = new BranchOffice("office 2", projectManager, resourceManager, userManager);
 	}
 
 	@After
@@ -33,33 +31,25 @@ public class BranchOfficeTest {
 		resourceManager = null;
 		userManager = null;
 		branchOffice1 = null;
-		branchOffice2 = null;
 	}
 
 	@Test
 	public void getNameTest() {
 		Assert.assertEquals("Incorrect name of office 1!", "office 1", branchOffice1.getName());
-		Assert.assertEquals("Incorrect name of office 2!", "office 2", branchOffice2.getName());
 	}
 
 	@Test
 	public void getProjectManagerTest() {
 		Assert.assertNotNull("Project manager cannot be null!", branchOffice1.getProjectManager());
-		Assert.assertNotNull("Project manager cannot be null!", branchOffice2.getProjectManager());
-		Assert.assertEquals("Incorrect project manager!", projectManager, branchOffice2.getProjectManager());
 	}
 
 	@Test
 	public void getResourceManagerTest() {
 		Assert.assertNotNull("Resource manager cannot be null!", branchOffice1.getResourceManager());
-		Assert.assertNotNull("Resource manager cannot be null!", branchOffice2.getResourceManager());
-		Assert.assertEquals("Incorrect resource manager!", resourceManager, branchOffice2.getResourceManager());
 	}
 
 	@Test
 	public void getUserManagerTest() {
 		Assert.assertNotNull("User manager cannot be null!", branchOffice1.getUserManager());
-		Assert.assertNotNull("User manager cannot be null!", branchOffice2.getUserManager());
-		Assert.assertEquals("Incorrect user manager!", userManager, branchOffice2.getUserManager());
 	}
 }
