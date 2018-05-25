b6b214dc238fc73ff74b1154398ac82952336808
Julien Benaouda <julien.benaouda@student.kuleuven.be>
2018-05-25 14:35:49 +0200

    small changes

--- src/taskman/backend/task/TaskStateUnavailable.java
+++ src/taskman/backend/task/TaskStateUnavailable.java
@@ -47,7 +47,6 @@ public class TaskStateUnavailable extends TaskState {
     public boolean canBePlanned() {
         return true;
     }
-    // TODO: moet isAvailable niet gechecked worden
 
     /**
      * Adds the given requirement to the task its requirements.
