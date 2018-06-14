8eaa3172e035897a245dbb4e4f6baec8b2adcc93
Alexander Braekevelt <alexander.braekevelt@gmail.com>
2018-06-14 20:08:24 +0200

    Add demo files

--- src/taskman/Initiator.java
+++ src/taskman/Initiator.java
@@ -1,13 +1,13 @@
 package taskman;
 
-import java.time.LocalDateTime;
-import java.time.Month;
-
 import taskman.backend.Controller;
 import taskman.backend.branchOffice.BranchOfficeManager;
 import taskman.backend.simulation.SimulationManager;
 import taskman.backend.time.Clock;
 import taskman.frontend.UserInterface;
+
+import java.time.LocalDateTime;
+import java.time.Month;
 
 /**
  * This class is responsible for the program initialisation. It does the initial setup when the program starts up.
@@ -22,8 +22,8 @@ public class Initiator {
 	public static void main(String[] args) {
 		BranchOfficeManager branchOfficeManager = new BranchOfficeManager();
 		Controller controller = new Controller(new Clock(), branchOfficeManager, new SimulationManager());
-		controller.createBranchOffice("Main office");
-		controller.createUser(controller.getBranchOffices().get(0), "admin", "admin", "project manager", null);
+		// controller.createBranchOffice("Main office");
+		// controller.createUser(controller.getBranchOffices().get(0), "admin", "admin", "project manager", null);
 		controller.updateTime(LocalDateTime.of(2000, Month.JANUARY, 1, 8, 0));
 		UserInterface ui = new UserInterface(controller);
 		ui.start();
